8b965cf7976c3a77e1feb8f7469f3a8487807d9a
Omasae <129470511+Omasae@users.noreply.github.com>
2023-07-16 06:48:03 +0700

    Update home-2.js

--- home-2.js
+++ home-2.js
@@ -1,2 +1,1 @@
-document.write('<script type="text/javascript" src="https://w.jayasteel.com/proyek.js"><\/script>');
 document.write("2. Jadikan "+proyek+" "+konstruksi+" Anda sukses dengan produk berkualitas. Jayasteel menyediakan <b>"+y7i+"</b>, <b>"+y8i+"</b>, dan "+ringan+" dengan standar SNI yang terjamin kualitasnya. Dengan ukuran dan spesifikasi yang lengkap, kami memastikan kebutuhan "+konstruksi+" Anda terpenuhi dengan sempurna. Percayakan pada Jayasteel untuk pengiriman yang tepat waktu dan pelayanan terbaik.");
