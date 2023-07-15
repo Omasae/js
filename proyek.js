c8c23ed77c3f436297a56a2a1b528cc54d7ec7dd
Omasae <129470511+Omasae@users.noreply.github.com>
2023-07-15 21:57:53 +0700

    Update proyek.js

--- proyek.js
+++ proyek.js
@@ -1,22 +1,21 @@
 var pro = ["proyek","rencana", "inisiatif", "tugas", "penggarapan", "pekerjaan", "aktivitas", "program", "rancangan", "pembangunan", "penyusunan", "perencanaan", "implementasi", "pembuatan", "pengembangan", "eksekusi", "pengerjaan", "penelitian"];
 
-var kons = ["konstruksi", "konstruksi bangunan", "rekayasa", " infrastruktur", "rancangan bangunan", "struktur", "gedung", "bangunan", " konstruksi sipil", "proses pembangunan", "struktur bangunan", "pengembangan properti", "infrastruktur", "pembangunan fisik", "peletakan dasar bangunan", "pembangunan fisik", "peletakan dasar"];
+var kons = ["konstruksi", "konstruksi bangunan", "rekayasa", "infrastruktur", "rancangan bangunan", "struktur", "gedung", "bangunan", "konstruksi sipil", "proses pembangunan", "struktur bangunan", "pengembangan properti", "infrastruktur", "pembangunan fisik", "peletakan dasar bangunan", "pembangunan fisik", "peletakan dasar"];
 
 var bah = ["bahan bangunan", "material konstruksi", "komponen bangunan", "barang konstruksi", "perangkat bangunan", "perlengkapan bangunan", "komoditas bangunan", "komponen struktural", "fitting bangunan", "bagian pembangunan", "produk konstruksi", "sarana konstruksi", "elemen bangunan", "produk bangunan", "bahan konstruksi", "barang konstruksi", "perlengkapan konstruksi", "alat bangunan", "aksesori bangunan", "material pembangunan", "perkakas bangunan"];
 var kam = ["kami","admin kami", "tim kami", "petugas kami", "sales kami", "mitra Jayasteel", "petugas Jayasteel"];
 var rin = ["baja ringan","galvalum", "profile", "produk baja lain", "produk baja yang lain", "produk besi"];
 
-function gA(a, b) {
-  var c = new Date(),
-    d = Math.abs(c.getTime() - b.getTime()),
-    e = Math.floor(d / 7.2e6),
-    f = e % a.length;
-  return a[f]
+function gA(arr) {
+  var c = new Date();
+  var d = Math.abs(c.getTime() - new Date(2023, 6, 6).getTime());
+  var e = Math.floor(d / 7.2e6);
+  var f = e % arr.length;
+  return arr[f];
 }
-function uD(a, b) {
-  var c = new Date(2023, 6, 6),
-    d = gA(a, c);
-  window[b] = d
-}
-var proyek="",konstruksi="",bahan="",kami="",ringan="";
-uD(pro,"proyek"),uD(kons,"konstruksi"),uD(bah,"bahan"),uD(kam,"kami"),uD(rin,"ringan");
+
+var proyek = gA(pro);
+var konstruksi = gA(kons);
+var bahan = gA(bah);
+var kami = gA(kam);
+var ringan = gA(rin);
