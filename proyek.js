855c943cdcbd390dacbf0303b16ddef5dc43e51e
Omasae <129470511+Omasae@users.noreply.github.com>
2023-07-16 12:34:29 +0700

    Update proyek.js

--- proyek.js
+++ proyek.js
@@ -3,5 +3,5 @@ var pro=["proyek","rencana","inisiatif","tugas","penggarapan","pekerjaan","aktiv
     bah=["bahan bangunan","material konstruksi","komponen bangunan","barang konstruksi","perangkat bangunan","perlengkapan bangunan","komoditas bangunan","komponen struktural","fitting bangunan","bagian pembangunan","produk konstruksi","sarana konstruksi","elemen bangunan","produk bangunan","bahan konstruksi","barang konstruksi","perlengkapan konstruksi","alat bangunan","aksesori bangunan","material pembangunan","perkakas bangunan"],
     kam=["kontak kami","admin kami","tim kami","petugas kami","sales kami","tim mitra Jayasteel","petugas Jayasteel"],
     rin=["baja ringan","galvalum","profile","produk baja lain","produk baja yang lain","produk besi"];
-function gA(ar){const f=Math.floor((new Date()-new Date(2023,7,7))/4e4)%arr.length;return ar[f];}
+function gA(ar){const f=Math.floor((new Date()-new Date(2023,7,7))/4e4)%ar.length;return ar[f];}
 var proyek=gA(pro),konstruksi=gA(kons),bahan=gA(bah),kami=gA(kam),ringan=gA(rin);
