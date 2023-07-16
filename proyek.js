050d7404a914cd36025a8d97803d95a5604b1e37
Omasae <129470511+Omasae@users.noreply.github.com>
2023-07-16 09:23:59 +0700

    Update proyek.js

--- proyek.js
+++ proyek.js
@@ -1,21 +1,7 @@
-var pro = ["proyek","rencana", "inisiatif", "tugas", "penggarapan", "pekerjaan", "aktivitas", "program", "rancangan", "pembangunan", "penyusunan", "perencanaan", "implementasi", "pembuatan", "pengembangan", "eksekusi", "pengerjaan", "penelitian"];
-
-var kons = ["konstruksi", "konstruksi bangunan", "rekayasa", "infrastruktur", "rancangan bangunan", "struktur", "gedung", "bangunan", "konstruksi sipil", "proses pembangunan", "struktur bangunan", "pengembangan properti", "infrastruktur", "pembangunan fisik", "peletakan dasar bangunan", "pembangunan fisik", "peletakan dasar"];
-
-var bah = ["bahan bangunan", "material konstruksi", "komponen bangunan", "barang konstruksi", "perangkat bangunan", "perlengkapan bangunan", "komoditas bangunan", "komponen struktural", "fitting bangunan", "bagian pembangunan", "produk konstruksi", "sarana konstruksi", "elemen bangunan", "produk bangunan", "bahan konstruksi", "barang konstruksi", "perlengkapan konstruksi", "alat bangunan", "aksesori bangunan", "material pembangunan", "perkakas bangunan"];
-var kam = ["kami","admin kami", "tim kami", "petugas kami", "sales kami", "mitra Jayasteel", "petugas Jayasteel"];
-var rin = ["baja ringan","galvalum", "profile", "produk baja lain", "produk baja yang lain", "produk besi"];
-
-function gA(arr) {
-  var c = new Date();
-  var d = Math.abs(c.getTime() - new Date(2023, 7, 7).getTime());
-  var e = Math.floor(d / 4e4);
-  var f = e % arr.length;
-  return arr[f];
-}
-
-var proyek = gA(pro);
-var konstruksi = gA(kons);
-var bahan = gA(bah);
-var kami = gA(kam);
-var ringan = gA(rin);
+var pro=["proyek","rencana","inisiatif","tugas","penggarapan","pekerjaan","aktivitas","program","rancangan","pembangunan","penyusunan","perencanaan","implementasi","pembuatan","pengembangan","eksekusi","pengerjaan","penelitian"],
+    kons=["konstruksi","konstruksi bangunan","rekayasa","infrastruktur","rancangan bangunan","struktur","gedung","bangunan","konstruksi sipil","proses pembangunan","struktur bangunan","pengembangan properti","infrastruktur","pembangunan fisik","peletakan dasar bangunan","pembangunan fisik","peletakan dasar"],
+    bah=["bahan bangunan","material konstruksi","komponen bangunan","barang konstruksi","perangkat bangunan","perlengkapan bangunan","komoditas bangunan","komponen struktural","fitting bangunan","bagian pembangunan","produk konstruksi","sarana konstruksi","elemen bangunan","produk bangunan","bahan konstruksi","barang konstruksi","perlengkapan konstruksi","alat bangunan","aksesori bangunan","material pembangunan","perkakas bangunan"],
+    kam=["kami","admin kami","tim kami","petugas kami","sales kami","mitra Jayasteel","petugas Jayasteel"],
+    rin=["baja ringan","galvalum","profile","produk baja lain","produk baja yang lain","produk besi"];
+function gA(ar){const f=Math.floor((new Date()-new Date(2023,7,7))/4e4)%arr.length;return ar[f];}
+var proyek=gA(pro),konstruksi=gA(kons),bahan=gA(bah),kami=gA(kam),ringan=gA(rin);
