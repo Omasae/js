2ecbae7b1f079705b74c84e5be4ba61772ea28e5
Omasae <129470511+Omasae@users.noreply.github.com>
2023-07-17 18:05:19 +0700

    Update coba.js

--- coba.js
+++ coba.js
@@ -1,8 +1,11 @@
-var v = ["1 untuk proyek Anda dan suplai distributor di Surabaya dan sekitarnya, atau tempat lain di Indonesia",
-"2 untuk proyek Anda dan suplai distributor di tempat Anda",
-"3 untuk proyek Anda dan suplai distributor di daerah Anda, serta informasi mengenai cara pemesanan, pengiriman, dan pembayaran",
-"4 update hari ini dan pemesanan"];
-document.addEventListener("DOMContentLoaded",()=>{
-  var e=Math.floor((Math.abs(new Date()-new Date(2023,6,6))/2e5)%v.length);
-  document.write(v[e])
+var v = [
+  "untuk proyek Anda dan suplai distributor di Surabaya dan sekitarnya, atau tempat lain di Indonesia",
+  "untuk proyek Anda dan suplai distributor di tempat Anda",
+  "untuk proyek Anda dan suplai distributor di daerah Anda, serta informasi mengenai cara pemesanan, pengiriman, dan pembayaran",
+  "update hari ini dan pemesanan"
+];
+
+document.addEventListener("DOMContentLoaded", function() {
+  var index = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 6, 6).getTime()) / 72e5) % v.length);
+  document.write(v[index]);
 });
