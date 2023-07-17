be489cf0e0dcb3ab4bdbd043ce02756c733fe30f
Omasae <129470511+Omasae@users.noreply.github.com>
2023-07-17 16:16:34 +0700

    Create coba.js

---
+++ coba.js
@@ -0,0 +1,5 @@
+var v = ["1 untuk proyek Anda dan suplai distributor di Surabaya dan sekitarnya, atau tempat lain di Indonesia",
+"2 untuk proyek Anda dan suplai distributor di tempat Anda",
+"3 untuk proyek Anda dan suplai distributor di daerah Anda, serta informasi mengenai cara pemesanan, pengiriman, dan pembayaran",
+"4 update hari ini dan pemesanan"];
+document.write(v[Math.floor((Math.abs(new Date().getTime()-new Date(2023,6,6).getTime())/2e5)%v.length)]);
