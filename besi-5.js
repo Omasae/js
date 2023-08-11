b71fbe738145993941f2ead3902bf7e59cca3b44
Omasae <129470511+Omasae@users.noreply.github.com>
2023-08-11 23:17:00 +0700

    Update besi-5.js

--- besi-5.js
+++ besi-5.js
@@ -1,1 +1,58 @@
-document.write("Mengingat pentingnya peran <b>besi beton</b> dalam konstruksi, tidak mengherankan bahwa kualitas menjadi faktor utama yang harus dipertimbangkan. Jayasteel sebagai pemasok terpercaya dalam industri baja tulangan, yang menyediakan produk berkualitas dengan standar SNI yang diperhatikan dengan sepenuh hati. Dengan mempercayakan kebutuhan konstruksi pada Jayasteel, Anda dapat memiliki keyakinan bahwa Anda menggunakan produk berkualitas yang memenuhi persyaratan teknis dan keselamatan yang baik.<br><b>Wiremesh</b> lembaran umumnya memiliki ukuran standar 2,1 x 5,4 meter. Dan, untuk diameter tulangan yang lebih kecil seperti M4, M5, dan M6, tersedia juga dalam bentuk gulungan dengan ukuran 2,1 x 54 meter. Hal ini memberikan fleksibilitas dalam memenuhi kebutuhan konstruksi yang beragam.");
+document.write("Mengingat pentingnya peran <b>besi beton</b> dalam konstruksi, tidak mengherankan bahwa kualitas menjadi faktor utama yang harus dipertimbangkan.");
+  
+var u1 = [" Jayasteel sebagai pemasok terpercaya dalam industri baja tulangan,",
+" Jayasteel, penyedia terkemuka dalam sektor baja tulangan,",
+" Sebagai pemain utama dalam industri baja tulangan, Jayasteel,",
+" Jayasteel, nama terpercaya dalam dunia industri baja tulangan,",
+" Dalam dunia industri besi beton, Jayasteel dikenal sebagai pemasok terpercaya,",
+" Jayasteel, perusahaan terkemuka di sektor baja tulangan,",
+" Terpercaya sebagai penyedia baja tulangan, Jayasteel,",
+" Jayasteel, pilihan utama dalam industri besi beton,",
+" Nama besar Jayasteel dalam industri baja tulangan,",
+" Jayasteel, mitra terpercaya dalam pasokan baja tulangan,",
+" Jayasteel, yang diakui dalam industri besi beton,",
+" Sebagai pemimpin industri baja tulangan, Jayasteel,",
+" Dalam pasokan baja tulangan, Jayasteel dikenal sebagai yang terbaik,"];
+var u2 = [" yang menyediakan produk berkualitas dengan standar SNI yang diperhatikan dengan sepenuh hati.",
+" yang menghadirkan produk bermutu sesuai standar SNI dengan perhatian yang mendalam.",
+" yang menawarkan produk unggul dengan memperhatikan standar SNI secara sungguh-sungguh.",
+" yang memberikan produk berkualitas baik dengan memperhatikan standar SNI dengan sungguh hati.",
+" yang menyajikan produk dengan kualitas terjaga sesuai standar SNI dan penuh perhatian.",
+" yang menghadirkan produk yang berkualitas baik dengan standar SNI yang diperhatikan sepenuhnya.",
+" yang menawarkan produk bermutu baik dengan standar SNI yang ditekankan secara serius.",
+" yang memberikan produk dengan standar SNI terjaga dan diperhatikan dengan sepenuh hati.",
+" yang menyediakan produk dengan kualitas terbaik dan standar SNI yang diutamakan.",
+" yang memberikan produk berkualitas baik dengan standar SNI yang ditekankan dengan sungguh-sungguh.",
+" yang menghadirkan produk yang memenuhi standar SNI dengan perhatian yang tulus.",
+" yang menawarkan produk bermutu baik dengan standar SNI yang diperhatikan secara serius.",
+" yang memberikan produk dengan standar SNI yang dijaga dengan sepenuh hati.",
+" yang menyediakan produk berkualitas sesuai standar SNI dan ditekankan dengan perhatian penuh.",
+" yang menghadirkan produk dengan kualitas terbaik dan standar SNI yang diperhatikan sepenuhnya."];
+var u3 = [" Dengan mempercayakan kebutuhan konstruksi pada Jayasteel,",
+" Dengan mengandalkan Jayasteel untuk keperluan konstruksi Anda,",
+" Dengan memercayakan pekerjaan konstruksi pada Jayasteel,",
+" Dengan mengandalkan Jayasteel untuk urusan konstruksi Anda,",
+" Dengan percaya pada Jayasteel dalam kebutuhan konstruksi,",
+" Dengan memilih Jayasteel untuk memenuhi kebutuhan konstruksi Anda,",
+" Dengan memberikan pilihan konstruksi pada Jayasteel,",
+" Dengan menjadikan Jayasteel sebagai pilihan dalam urusan konstruksi Anda,",
+" Dengan memilih Jayasteel sebagai mitra untuk keperluan konstruksi,",
+" Dengan menugaskan Jayasteel untuk memenuhi kebutuhan konstruksi,",
+" Dengan mengandalkan Jayasteel dalam melaksanakan proyek konstruksi,"];
+var u4 = [" Anda dapat memiliki keyakinan bahwa Anda menggunakan produk yang memenuhi persyaratan teknis dan keselamatan yang baik.",
+" Anda bisa yakin bahwa produk yang digunakan telah sesuai dengan persyaratan teknis dan standar keselamatan.",
+" Anda dapat mempercayai bahwa produk yang Anda gunakan telah memenuhi persyaratan teknis dan standar keselamatan yang oke.",
+" Anda memiliki keyakinan bahwa produk yang digunakan telah memenuhi persyaratan teknis dan standard keselamatan.",
+" Anda bisa tenang karena produk yang digunakan telah memenuhi persyaratan teknis dan standar keselamatan yang optimal.",
+" Anda bisa merasa yakin bahwa produk yang digunakan memenuhi persyaratan teknis dan standar keselamatan baik.",
+" Anda bisa memiliki keyakinan bahwa produk yang digunakan telah memenuhi standar teknis dan keselamatan dengan baik.",
+" Anda dapat yakin bahwa produk yang digunakan telah memenuhi persyaratan teknis dan standar keselamatan yang terjaga.",
+" Anda bisa percaya bahwa produk yang digunakan telah memenuhi standar teknis dan keselamatan yang baik."];
+          
+var e1 = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 6, 16).getTime()) / 39e6) % u1.length);
+var e2 = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 6, 26).getTime()) / 62e6) % u2.length);
+var e3 = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 6, 12).getTime()) / 122e6) % u3.length);
+var e4 = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 7, 3).getTime()) / 72e6) % u4.length);
+document.write(u1[e1]+u2[e2]+u3[e3]+u4[e4]);
+  
+document.write("<br><b>Wiremesh</b> lembaran umumnya memiliki ukuran standar 2,1 x 5,4 meter. Dan, untuk diameter tulangan yang lebih kecil seperti M4, M5, dan M6, tersedia juga dalam bentuk gulungan dengan ukuran 2,1 x 54 meter. Hal ini memberikan fleksibilitas dalam memenuhi kebutuhan konstruksi yang beragam.");
