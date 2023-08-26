930cc9fc83639434d56f95e6754b1f879bc09acb
Omasae <129470511+Omasae@users.noreply.github.com>
2023-08-27 00:01:49 +0700

    Update home-9.js

--- home-9.js
+++ home-9.js
@@ -1,1 +1,5 @@
-document.write("9. Memperkuat fondasi "+proyek+" Anda dengan <b>"+y7i+"</b>, <b>"+y8i+"</b>, dan "+ringan+" berkualitas. Dalam berbagai ukuran dan spesifikasi, Jayasteel menyediakan produk yang tepat untuk kebutuhan "+konstruksi+" Anda. Dapatkan pengiriman yang cepat dan efisien ke lokasi "+proyek+" Anda. Jayasteel berkomitmen untuk memberikan kepuasan kepada konsumen dengan produk terbaik dan layanan yang dapat dipercaya. Hubungi "+kami+".");
+var u = ["9. Memperkuat fondasi '+proyek+' Anda dengan <b>'+y7i+'</b>, <b>'+y8i+'</b>, dan '+ringan+' berkualitas. Dalam berbagai ukuran dan spesifikasi, Jayasteel menyediakan produk yang tepat untuk kebutuhan '+konstruksi+' Anda. Dapatkan pengiriman yang cepat dan efisien ke lokasi '+proyek+' Anda. Jayasteel berkomitmen untuk memberikan kepuasan kepada konsumen dengan produk terbaik dan layanan yang dapat dipercaya. Hubungi '+kami+'.",
+" ",
+" "];
+var e = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 7, 6).getTime()) / 112e6) % u.length);
+document.write(u[e]);
