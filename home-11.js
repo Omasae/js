d6cf91146f28989ba9e2353b2681b1c0701e6342
Omasae <129470511+Omasae@users.noreply.github.com>
2023-08-30 05:32:44 +0700

    Update home-11.js

--- home-11.js
+++ home-11.js
@@ -1,5 +1,5 @@
 var u = ["11. Jadikan "+proyek+" "+konstruksi+" Anda lebih kuat dan tahan lama dengan <b>"+y7i+"</b>, <b>"+y8i+"</b>, dan "+ringan+" berkualitas. Dengan berbagai ukuran dan spesifikasi yang tersedia, Jayasteel dapat memenuhi kebutuhan "+konstruksi+" Anda dengan tepat. Pengiriman produk yang cepat dan pelayanan yang ramah adalah yang Jayasteel prioritaskan. Dapatkan barang berkualitas dan pelayanan terbaik. Hubungi "+kami+".",
 " ",
 " "];
-var e = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 7, 8).getTime()) / 112e6) % u.length);
+var e = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 7, 12).getTime()) / 112e6) % u.length);
 document.write(u[e]);
