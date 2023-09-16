478b94d5b3488bfd13071575307b3248cb7cf5c0
Omasae <129470511+Omasae@users.noreply.github.com>
2023-09-16 11:11:10 +0700

    Update home-4.js

--- home-4.js
+++ home-4.js
@@ -1,1 +1,5 @@
-document.write("4. Selain <b>"+y7i+"</b>, <b>"+y8i+"</b>, dan "+ringan+", Jayasteel juga menyediakan berbagai produk update "+bul+" "+thn+": "+bahan+" lainnya. Apapun kebutuhan "+konstruksi+" Anda, sampaikan kepada "+kami+" dan kami akan mencarikan solusinya. Dengan mitra yang handal dan berpengalaman, Jayasteel dapat menyediakan produk baja dan update "+bul+" "+thn+": "+bahan+" lainnya untuk "+proyek+" besar hingga kebutuhan harian. Kami berkomitmen memberikan pelayanan terbaik dan memenuhi ekspektasi Anda.");
+var u = [" 4. Selain <b>"+y7i+"</b>, <b>"+y8i+"</b>, dan "+ringan+", Jayasteel juga menyediakan berbagai produk update "+bul+" "+thn+": "+bahan+" lainnya. Apapun kebutuhan "+konstruksi+" Anda, sampaikan kepada "+kami+" dan kami akan mencarikan solusinya. Dengan mitra yang handal dan berpengalaman, Jayasteel dapat menyediakan produk baja dan update "+bul+" "+thn+": "+bahan+" lainnya untuk "+proyek+" besar hingga kebutuhan harian. Kami berkomitmen memberikan pelayanan terbaik dan memenuhi ekspektasi Anda.",
+". ",
+".. "];
+var e=Math.floor((Math.abs(new Date().getTime()-new Date(2023,8,6).getTime())/155e6)%u.length);
+document.write(u[e]);
