41a4f0b972e858368f6a267a220eb15c668237e1
Omasae <129470511+Omasae@users.noreply.github.com>
2023-10-20 18:22:16 +0700

    Update besi-10.js

--- besi-10.js
+++ besi-10.js
@@ -1,1 +1,16 @@
-document.write("Bagaimana cara pembuatan besi beton?<br />Proses pembuatan <b>besi beton</b> dimulai dengan menggunakan rolling mill. Bahan baku berupa billet (baja dalam bentuk kotak panjang) dipanaskan hingga mencapai suhu yang hampir mencapai titik lembutnya. Setelah itu, billet tersebut digiling menggunakan roll yang berputar. Akibat tekanan dan tarikan yang diberikan, billet berubah bentuk menjadi lebih kecil dan lebih panjang. Proses rolling dilakukan berulang kali dengan menggunakan caliber tertentu, hingga mencapai ukuran <b>besi beton</b> yang diinginkan.");
+var u = [" Proses rolling dilakukan berulang kali dengan menggunakan caliber tertentu,",
+" Caliber tertentu digunakan dalam proses rolling yang diulang berulang kali,",
+" Dalam proses rolling, caliber tertentu digunakan secara berulang-ulang,",
+" Proses rolling dilakukan berkali-kali dengan menggunakan caliber khusus,",
+" Caliber tertentu digunakan berulang kali dalam proses rolling,",
+" Dalam proses rolling, penggunaan caliber tertentu diulang secara terus-menerus,",
+" Proses rolling dilakukan secara berulang dengan menggunakan caliber tertentu,",
+" Caliber khusus digunakan berulang kali dalam proses rolling,",
+" Dalam proses rolling, caliber tertentu diterapkan secara berulang-ulang,",
+" Proses rolling dilaksanakan berulang kali dengan menggunakan caliber spesifik,",
+" Caliber tertentu dipakai berulang-ulang dalam proses rolling,",
+" Dalam proses rolling, penggunaan caliber tertentu diulang berkali-kali,",
+" Proses rolling dijalankan berulang kali menggunakan caliber tertentu,"];
+var d = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 6, 6).getTime()) / 3e7) % u.length);
+document.write(u[d]+v[e]);
+document.write("Bagaimana cara pembuatan besi beton?<br />Proses pembuatan <b>besi beton</b> dimulai dengan menggunakan rolling mill. Bahan baku berupa billet (baja dalam bentuk kotak panjang) dipanaskan hingga mencapai suhu yang hampir mencapai titik leburnya. Setelah itu, billet tersebut digiling menggunakan roll yang berputar. Akibat tekanan dan tarikan yang diberikan, billet berubah bentuk menjadi lebih kecil dan lebih panjang."+u[d]+"hingga mencapai ukuran <b>besi beton</b> yang diinginkan.");
