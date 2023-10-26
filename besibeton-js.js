c15e3081d3b77fb7fbe015b9b030d34e1b734dc2
Omasae <129470511+Omasae@users.noreply.github.com>
2023-10-26 11:29:20 +0700

    Create besibeton-js.js

---
+++ besibeton-js.js
@@ -0,0 +1,172 @@
+var f = [" kualitas",
+" mutu",
+" standard kualitas",
+" standar kualitas",
+" kualitas yang",
+" standar mutu"];
+var g =((Math.floor((new Date()-(tgT-2))/31e7))%f.length);
+var h = [" terbaik",
+" prima",
+" unggul",
+" terunggul",
+" teratas",
+" terdepan",
+" premium",
+" tinggi",
+" optimal"];
+var i=((Math.floor((new Date()-(tgT-5))/37e7))% h.length);
+var j = [" dan",
+" serta"];
+var k=((Math.floor((new Date()-(tgT-4))/19e7))% j.length);
+var l = [" kekuatan",
+" ketangguhan"];
+var m=((Math.floor((new Date()-(tgT-26))/16e7))% l.length);
+var n = [" mumpuni,",
+" tangguh,",
+" handal,",
+" andal,",
+" baik,",
+" tahan lama,",
+" memukau,",
+" memastikan,",
+" terjamin,",
+" kokoh,",
+" tahan lama,",
+" tidak diragukan,",
+" terpercaya,",
+" sangat handal,"];
+var o=((Math.floor((new Date()-(tgT-23))/27e7))%n.length);
+var u = [" besi beton",
+" Besi Tulangan",
+" Besi Cor",
+" Tulangan Baja",
+" Tulangan Beton",
+" Baja Tulangan",
+" Baja Cor (besi beton)",
+" Besi Bangunan (besi beton)",
+" Besi Struktural (besi beton)",
+" Besi Konstruksi (besi beton)",
+" Tulangan Besi",
+" Tulangan Konstruksi",
+" Baja Struktural",
+" Baja Konstruksi",
+" Tulangan Baja Beton",
+" Besi Konkrit (besi beton)",
+" Besi Struktural Baja",
+" Besi Bangunan Baja",
+" Tulangan Baja Bangunan",
+" Tulangan Baja Konstruksi",
+" Rebar",
+" Tulangan Baja Struktural",
+" Tulangan Beton Bertulang",
+" Tulangan Bangunan Baja",
+" Besi Konstruksi Bertulang (besi beton)",
+" Besi Beton Bertulang",
+" Tulangan Baja Beton Bertulang (besi beton)",
+" Besi Baja Tulangan",
+" Besi Baja Konstruksi (besi beton)",
+" betoneser (besi beton)"];
+var d=((Math.floor((new Date()-(tgT-8))/97e6))%u.length);
+var p = [" Jayasteel",
+" Jaya Steel",
+" Mitra Jayasteel",
+" Mitra Jaya Steel"];
+var q=((Math.floor((new Date()-(tgT-4))/197e6))%l.length);
+var r = [" adalah",
+" ialah",
+" merupakan",
+" yaitu",
+" adalah merupakan",
+" yakni"];
+var s=((Math.floor((new Date()-(tgT-12))/237e6))%l.length);
+var v = [" pilihan",
+" alternatif",
+" opsi",
+" solusi"];
+var w=((Math.floor((new Date()-(tgT-11))/233e6))%l.length);
+var x = [" utama",
+" terbaik",
+" teratas",
+" pertama",
+" terunggul",
+" paling baik",
+" terfavorit"];
+var y=((Math.floor((new Date()-(tgT-10))/99e6))%l.length);
+var z = [" untuk",
+" bagi",
+" sebagai",
+" untuk keperluan",
+" sehubungan dengan",
+" berkenaan dengan",
+" guna",
+" dalam hal"];
+var z1=((Math.floor((new Date()-(tgT-19))/67e6))%l.length);
+var a1 = [" proyek",
+" pekerjaan",
+" pekerjaan proyek"];
+var a2=((Math.floor((new Date()-(tgT-18))/78e6))%l.length);
+var b1 = [" konstruksi",
+" bangunan",
+" pembangunan",
+" infrastruktur",
+" rekayasa",
+" struktural",
+" konstruksi sipil",
+" bangunan dan infrastruktur",
+" pembangunan fisik",
+" pembangunan struktural",
+" konstruksi bangunan",
+" konstruksi bangunan dan infrastruktur",
+" konstruksi fisik",
+" pembangunan sipil",
+" konstruksi rekayasa",
+" pembangunan rekayasa",
+" pembangunan struktural dan infrastruktur",
+" konstruksi struktural"];
+var b2=((Math.floor((new Date()-(tgT-17))/97e6))%l.length);
+var c1 = [" Anda",
+""];
+var c2=((Math.floor((new Date()-(tgT-15))/57e6))%l.length);
+var d1 = [" memastikan",
+" menjamin",
+" menyakinkan",
+" memverifikasi",
+" meyakinkan",
+" menegaskan",
+" menetapkan",
+" mengonfirmasi"];
+var d2=((Math.floor((new Date()-(tgT-21))/497e6))%l.length);
+var e1 = [" keandalan",
+" ketepatan",
+" keterpercayaan",
+" ketangguhan",
+" kemampuan",
+" kehandalan",
+" ketahanan",
+" kekokohan",
+" kestabilan",
+" keteguhan"];
+var e2=((Math.floor((new Date()-(tgT-11))/797e6))%l.length);
+var f1 = [" struktur bangunan",
+" rangka bangunan",
+" arsitektur",
+" konstruksi",
+" komponen struktural",
+" struktur fisik bangunan",
+" sistem konstruksi",
+" bagian konstruksi",
+" rancang bangun",
+" bentuk arsitektural"];
+var f2=((Math.floor((new Date()-(tgT-21))/366e6))%l.length);
+var g1 = [" selamanya",
+" untuk selamanya",
+" sepanjang masa",
+" abadi",
+" selalu",
+" selama-lamanya",
+" senantiasa",
+" terus-menerus",
+" sepanjang waktu",
+" untuk selama-lamanya"];
+var g2=((Math.floor((new Date()-(tgT-6))/497e6))%l.length);
+document.write("Dengan"+f[g]+h[i]+j[k]+l[m]+" yang"+n[o]+u[d]+p[q]+r[s]+v[w]+x[y]+z[z1]+a1[a2]+b1[b2]+c1[c2]+","+d1[d2]+e1[e2]+f1[f2]+c1[c2]+g1[g2]+".");
