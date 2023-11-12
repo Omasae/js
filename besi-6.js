7b634486e73825fea3bf323d17c123415e22f43a
Omasae <129470511+Omasae@users.noreply.github.com>
2023-11-13 04:57:42 +0700

    Update besi-6.js

--- besi-6.js
+++ besi-6.js
@@ -1,1 +1,5 @@
-document.write("<b>Besi beton</b> atau baja tulangan memiliki peran yang sangat penting dalam meningkatkan kekuatan tarik struktur beton. Penggunaan <b>besi beton</b> yang tepat dengan spesifikasi yang sesuai dan penerapan yang benar dapat memberikan kestabilan dan kekokohan struktur yang diperlukan dalam berbagai proyek konstruksi. Oleh karena itu, pastikan Anda bekerja sama dengan konsultan yang baik dan memilih produsen atau pemasok yang terpercaya untuk memenuhi kebutuhan konstruksi Anda.");
+var u = [" <b>Besi beton</b> atau baja tulangan memiliki peran yang sangat penting dalam meningkatkan kekuatan tarik struktur beton. Penggunaan <b>besi beton</b> yang tepat dengan spesifikasi yang sesuai dan penerapan yang benar dapat memberikan kestabilan dan kekokohan struktur yang diperlukan dalam berbagai proyek konstruksi. Oleh karena itu, pastikan Anda bekerja sama dengan konsultan yang baik dan memilih produsen atau pemasok yang terpercaya untuk memenuhi kebutuhan konstruksi Anda.",
+" Info tentang Besi Beton dan Wiremesh."," ",
+" "];
+var e = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 9, 6).getTime()) / 499e6) % u.length);
+document.write(u[e]);
