5b0f9bb7c1ce2a86aa8f9a1944f7b166e48cc476
Omasae <129470511+Omasae@users.noreply.github.com>
2023-11-13 05:31:08 +0700

    Update home-17.js

--- home-17.js
+++ home-17.js
@@ -1,1 +1,5 @@
-document.write("17. Produk update "+bul+" "+thn+": "+bahan+" yang tepat untuk "+proyek+" Anda. Jayasteel menyediakan <b>"+y7i+"</b>, <b>"+y8i+"</b>, "+ringan+", dan banyak lagi. Dapatkan produk berkualitas dengan harga yang kompetitif. Layanan pengiriman kami mencakup seluruh wilayah Indonesia, sehingga Anda dapat mengandalkan kami untuk memenuhi kebutuhan "+proyek+" Anda. Kepercayaan pelanggan adalah prioritas utama kami.");
+var u = [" Produk update "+bul+" "+thn+": "+bahan+" yang tepat untuk "+proyek+" Anda. Jayasteel menyediakan <b>"+y7i+"</b>, <b>"+y8i+"</b>, "+ringan+", dan banyak lagi. Dapatkan produk berkualitas dengan harga yang kompetitif. Layanan pengiriman kami mencakup seluruh wilayah Indonesia, sehingga Anda dapat mengandalkan kami untuk memenuhi kebutuhan "+proyek+" Anda. Kepercayaan pelanggan adalah prioritas utama kami.",
+" ...Besi Beton - Wiremesh..."," ",
+" "];
+var e = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 9, 6).getTime()) / 499e6) % u.length);
+document.write(u[e]);
