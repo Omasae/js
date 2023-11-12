d0e91edfc737cc720d6fb0cd00922155d98b4be7
Omasae <129470511+Omasae@users.noreply.github.com>
2023-11-13 05:30:03 +0700

    Update home-16.js

--- home-16.js
+++ home-16.js
@@ -1,1 +1,5 @@
-document.write("16. Jadikan "+proyek+" "+konstruksi+" Anda lebih efisien dengan produk berkualitas dari Jayasteel. Jayasteel menyediakan <b>"+y7i+"</b>, <b>"+y8i+"</b>, dan "+ringan+" dengan berbagai ukuran dan spesifikasi yang lengkap. Dapatkan produk yang tahan lama, kuat, dan mudah digunakan. Pengiriman cepat dan layanan pelanggan yang ramah adalah yang kami utamakan. Percayakan kebutuhan update "+bul+" "+thn+": "+bahan+" Anda kepada kami.");
+var u = [" Jadikan "+proyek+" "+konstruksi+" Anda lebih efisien dengan produk berkualitas dari Jayasteel. Jayasteel menyediakan <b>"+y7i+"</b>, <b>"+y8i+"</b>, dan "+ringan+" dengan berbagai ukuran dan spesifikasi yang lengkap. Dapatkan produk yang tahan lama, kuat, dan mudah digunakan. Pengiriman cepat dan layanan pelanggan yang ramah adalah yang kami utamakan. Percayakan kebutuhan update "+bul+" "+thn+": "+bahan+" Anda kepada kami.",
+" ..."," ",
+" "];
+var e = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 9, 6).getTime()) / 499e6) % u.length);
+document.write(u[e]);
