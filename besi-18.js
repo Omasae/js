454c2b5ae529c0fd007a5701533753598dd6823f
Omasae <129470511+Omasae@users.noreply.github.com>
2023-11-13 04:51:55 +0700

    Update besi-18.js

--- besi-18.js
+++ besi-18.js
@@ -1,1 +1,5 @@
-document.write("Apa itu besi beton?<br />Besi beton adalah jenis baja yang digunakan sebagai tulangan untuk memperkuat struktur beton.<br />Apa itu besi beton ulir?<br />Besi beton ulir adalah jenis besi beton yang memiliki bentuk ulir. Bentuk ulir ini membantu meningkatkan kekuatan dan stabilitas struktur beton. Ya, <b>besi beton ulir</b> juga dapat melekat lebih kuat pada beton karena bentuk ulir atau siripnya yang dapat mencengkram dengan baik.");
+var u = [" Apa itu besi beton?<br />Besi beton adalah jenis baja yang digunakan sebagai tulangan untuk memperkuat struktur beton.<br />Apa itu besi beton ulir?<br />Besi beton ulir adalah jenis besi beton yang memiliki bentuk ulir. Bentuk ulir ini membantu meningkatkan kekuatan dan stabilitas struktur beton. Ya, <b>besi beton ulir</b> juga dapat melekat lebih kuat pada beton karena bentuk ulir atau siripnya yang dapat mencengkram dengan baik.",
+" Berbagi info tentang Besi Beton dan Wiremesh."," ",
+" "];
+var e = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 9, 6).getTime()) / 382e6) % u.length);
+document.write(u[e]);
