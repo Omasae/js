7dc0b65c8578f3501fccabbb6b06261a97981183
Omasae <129470511+Omasae@users.noreply.github.com>
2024-03-03 20:58:18 +0700

    Update home-9.js

--- home-9.js
+++ home-9.js
@@ -1,4 +1,4 @@
-var u = [" Memperkuat fondasi '+proyek+' Anda dengan <b>'+y7i+'</b>, <b>'+y8i+'</b>, dan '+ringan+' berkualitas. Dalam berbagai ukuran dan spesifikasi, Jayasteel menyediakan produk yang tepat untuk kebutuhan '+konstruksi+' Anda. Dapatkan pengiriman yang cepat dan efisien ke lokasi '+proyek+' Anda. Jayasteel berkomitmen untuk memberikan kepuasan kepada konsumen dengan produk terbaik dan layanan yang dapat dipercaya. Hubungi '+kami+'.",
+var u = [" Memperkuat fondasi proyek Anda dengan besi beton wiremesh berkualitas. Dalam berbagai ukuran dan spesifikasi, Jayasteel menyediakan produk yang tepat untuk kebutuhan '+konstruksi+' Anda. Dapatkan pengiriman yang cepat dan efisien ke lokasi proyek Anda. Jayasteel berkomitmen untuk memberikan kepuasan kepada konsumen dengan produk terbaik dan layanan yang dapat dipercaya. Hubungi admin.",
 " ",
 " "];
 var e = Math.floor((Math.abs(new Date().getTime() - new Date(2023, 7, 6).getTime()) / 112e6) % u.length);
